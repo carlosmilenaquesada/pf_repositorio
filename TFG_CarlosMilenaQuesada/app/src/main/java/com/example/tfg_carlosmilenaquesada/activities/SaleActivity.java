a76b3a246cc56f5cde7804b9956594b268d13b48
carlosmilenaquesada <135043776+carlosmilenaquesada@users.noreply.github.com>
2024-04-07 22:47:12 +0200

    ultimo

--- TFG_CarlosMilenaQuesada/app/src/main/java/com/example/tfg_carlosmilenaquesada/activities/SaleActivity.java
+++ TFG_CarlosMilenaQuesada/app/src/main/java/com/example/tfg_carlosmilenaquesada/activities/SaleActivity.java
@@ -4,9 +4,12 @@ import static com.example.tfg_carlosmilenaquesada.database.DbHelper.TABLE_CUSTOM
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,8 +20,10 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.tfg_carlosmilenaquesada.database.DbHelper;
 
 import com.example.tfg_carlosmilenaquesada.R;
+import com.example.tfg_carlosmilenaquesada.models.CustomerType;
 
 public class SaleActivity extends AppCompatActivity {
+    Spinner spCustomersTypes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +36,26 @@ public class SaleActivity extends AppCompatActivity {
             return insets;
         });
 
-        Spinner spinner = findViewById(R.id.spCustomersTypes);
-        System.out.println("hola");
+        spCustomersTypes = findViewById(R.id.spCustomersTypes);
         Cursor cursorCustomersTypes = MainActivity.getDbHelper().getReadableDatabase().rawQuery("SELECT description as _id FROM " + TABLE_CUSTOMERS_TYPES, null);
-        String[] fromColumns = { "_id" };
-        int[] toViews = { android.R.id.text1 };
+        String[] fromColumns = {"_id"};
+        int[] toViews = {android.R.id.text1};
         CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_item, cursorCustomersTypes, fromColumns, toViews, 0);
-        spinner.setAdapter(cursorAdapter);
+        spCustomersTypes.setAdapter(cursorAdapter);
+        spCustomersTypes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int showEtCustomerId = ((Cursor) spCustomersTypes.getAdapter().getItem(position)).getString(0).equals("Cliente fiscal") ?
+                        View.VISIBLE : View.GONE;
+                findViewById(R.id.etCustomerId).setVisibility(showEtCustomerId);
 
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
 }
